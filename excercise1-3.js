0acbf14f0c9a1f7b9e79c89626632561736442ee
LukeJ10 <lukas.jiun@gmail.com>
2024-05-03 17:04:41 +0700

    tambah nomer 3 versi ngecheat

--- excercise1-3.js
+++ excercise1-3.js
@@ -4,3 +4,9 @@ for (var i = 0; i <=10; i++)
     {
         console.log( `${numberToMultiply}`, "X" ,`${i}`)
     }
+
+
+
+const paragraph = "hello world!";
+const replacer = "ell"
+console.log(paragraph.replace(replacer,""))
