b8415b708e17afa7b1e35147f227c8f2866623d9
LukeJ10 <lukas.jiun@gmail.com>
2024-05-03 17:19:43 +0700

    tambah nomer 7 dua cara tapi aneh

--- excercise1-3.js
+++ excercise1-3.js
@@ -10,3 +10,15 @@ for (var i = 0; i <=10; i++)
 const paragraph = "hello world!";
 const replacer = "ell"
 console.log(paragraph.replace(replacer,""));
+
+const kataDibalik = "testing";
+let arrayKata = [];
+
+for(var i = kataDibalik.length-1; i>=0 ; i--) {
+    console.log(kataDibalik[i])
+}
+for (var i = kataDibalik.length-1; i>=0 ; i--) {
+    arrayKata.push(kataDibalik[i])
+}
+
+console.log(arrayKata)
